cd966207b31e044139b9f542fea59f19fb745b6c
sasholk <iccc97@gmail.com>
2022-01-19 18:27:12 +0200

    created filter

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -18,7 +18,8 @@ export class App extends Component {
                 { name: 'Mark V.', salary: 3000, increase: true, rise: false, id: 2 },
                 { name: 'Alex B.', salary: 5000, increase: false, rise: false, id: 3 },
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -104,11 +105,26 @@ export class App extends Component {
         this.setState({ term });
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({ filter });
+    }
+
     render() {
-        const { data, term } = this.state;
+        const { data, term, filter } = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app" >
@@ -116,7 +132,7 @@ export class App extends Component {
 
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-                    <AppFilter />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
                 </div>
 
                 <EmployeesList
