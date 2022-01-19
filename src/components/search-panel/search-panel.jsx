a7d1d968d6c720c04b0f0b22759fece25ce8b340
sasholk <iccc97@gmail.com>
2022-01-19 15:54:53 +0200

    created search

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,10 +1,29 @@
+import { Component } from 'react';
+
 import './search-panel.css';
 
-export const SearchPanel = () => {
-    return (
-        <input
-            type="text"
-            className="form-control search-input"
-            placeholder="Найти сотрудника" />
-    )
+export class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearch = (e) => { // эта часть работает локально 
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onUpdateSearch(term) // onUpdateSearch приходит и другого компонента 
+    }
+
+    render() {
+        return (
+            <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch} />
+        )
+    }
 }
